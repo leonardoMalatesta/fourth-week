d8b76f51a7eae2ab84d2e6b33cb7c5eeb5df4336
Leonardo <leonardomalatesta18@gmail.com>
2022-04-12 07:48:30 +0200

    Aggiornamento 1 Secondo Angular

--- second-angular/src/app/model/device.ts
+++ second-angular/src/app/model/device.ts
@@ -1,9 +1,9 @@
-export interface Device{
-    id?:number;
+export interface Device {
+    id?: number;
     label?: string;
     os?: string;
     price?: number;
     memory?: number;
     rate?: number;
     desc?: string;
-}
+  }
